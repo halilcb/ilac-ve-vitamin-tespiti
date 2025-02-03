aca316468f8dc1e0b56c273c5234123976dc41c6
Halil <121048400+halilcb@users.noreply.github.com>
2025-02-03 09:31:05 +0300

    ilac-ve-vitamin-tespiti.py

--- ilac-ve-vitamin-tespiti.py
+++ ilac-ve-vitamin-tespiti.py
@@ -52,12 +52,11 @@ for i, ax in enumerate(axes.flat):
 
 plt.tight_layout()
 
-#%% data preprocessing: train-test split, data augmentation, resize, rescaling
+#%% ---------- Data Preprocessing ----------
 
-# train-test split
+
 train_df, test_df = train_test_split(image_df, test_size=0.2, shuffle=True, random_state=42)
 
-# data augmentation: veri artırımı
 train_generator = ImageDataGenerator(
     preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input,
     validation_split=0.2
@@ -69,9 +68,9 @@ test_generator = ImageDataGenerator(
 
 train_images = train_generator.flow_from_dataframe(
     dataframe=train_df,
-    x_col="filepath",  # independent -> görüntü
-    y_col="label",     # dependent -> target variable -> etiket
-    target_size=(224, 224),  # görüntülerin boyutu
+    x_col="filepath",  
+    y_col="label",     
+    target_size=(224, 224),  
     color_mode="rgb",
     class_mode="categorical",
     batch_size=16,
@@ -104,64 +103,114 @@ test_images = test_generator.flow_from_dataframe(
     shuffle=False
 )
 
-# resize, rescale
 resize_and_rescale = tf.keras.Sequential([
     layers.Resizing(224, 224),
     layers.Rescaling(1./255)
 ])
 
 
+# %% ---------- Transfer Learning (MobileNetV2) ----------
+
+pretrained_model = tf.keras.applications.MobileNetV2(  
+    input_shape=(224, 224, 3),  
+    include_top=False,          
+    weights="imagenet",         
+    pooling="avg"
+)
+
+pretrained_model.trainable = False
 
 
+checkpoint_path = "pharmaceutical_drugs_and_vitamins_classification_model_checkpoint.weights.h5"
+checkpoint_callback = ModelCheckpoint(
+    checkpoint_path,
+    save_weights_only=True,
+    monitor="val_accuracy",
+    save_best_only=True
+)
+
+early_stopping = EarlyStopping(
+    monitor="val_loss",
+    patience=5,
+    restore_best_weights=True
+)
+
+# training model
+inputs = pretrained_model.input
+x = resize_and_rescale(inputs)
+x = Dense(256, activation="relu")(pretrained_model.output)
+x = Dropout(0.2)(x)
+x = Dense(256, activation="relu")(x)
+x = Dropout(0.2)(x)
+outputs = Dense(10, activation="softmax")(x)
+model = Model(inputs=inputs, outputs=outputs)
+
+model.compile(optimizer=Adam(0.0001), loss="categorical_crossentropy", metrics=["accuracy"])
+
+history = model.fit(
+    train_images,
+    steps_per_epoch=len(train_images),
+    validation_data=val_images,
+    validation_steps=len(val_images),
+    epochs=50,
+    callbacks=[early_stopping, checkpoint_callback]
+)
 
 
+#%% ---------- Model Evaluation ----------
 
+results = model.evaluate(test_images, verbose=1)
 
+print("Test Loss: ", results[0])
+print("Test Accuracy: ", results[1])
 
+epochs = range(1, len(history.history["accuracy"]) + 1)
+hist = history.history
 
+plt.figure(figsize=(12, 5))
 
+plt.subplot(1, 2, 1)
+plt.plot(epochs, hist["accuracy"], "bo-", label="Training Accuracy")
+plt.plot(epochs, hist["val_accuracy"], "r^-", label="Validation Accuracy")
+plt.title("Training and Validation Accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.grid(True)
+plt.legend()
 
+plt.subplot(1, 2, 2)
+plt.plot(epochs, hist["loss"], "bo-", label="Training Loss")
+plt.plot(epochs, hist["val_loss"], "r^-", label="Validation Loss")
+plt.title("Training and Validation Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.grid(True)
+plt.legend()
 
+plt.tight_layout()
+plt.show()
 
+# %% ---------- Model Testing ----------
 
+pred = model.predict(test_images)
+pred = np.argmax(pred, axis=1)
 
+labels = (train_images.class_indices)
+labels = dict((v, k) for k, v in labels.items())
+pred = [labels[k] for k in pred]
 
+random_index = np.random.randint(0, len(test_df), 15)
+fig, axes = plt.subplots(nrows=5, ncols=3, figsize=(11, 11))
 
+for i, ax in enumerate(axes.flat):
+    ax.imshow(plt.imread(image_df.filepath[random_index[i]]))
+    if test_df.label.iloc[random_index[i]] == pred[random_index[i]]:
+        color = "green"
+    else:
+        color = "red"
+    ax.set_title(f"True: {test_df.label.iloc[random_index[i]]}\nPredicted: {pred[random_index[i]]}", color=color)
 
+plt.tight_layout()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+y_test = list(test_df.label)
+print(classification_report(y_test, pred))
